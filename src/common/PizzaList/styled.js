bc7b521c9a231d6984e44416ec88477d6e86e293
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-24 21:20:01 +0100

    add styling for pizzalist

---
+++ src/common/PizzaList/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin-top: 100px;
+  padding: 20px;
+`;
+
+export const Header = styled.h2``;
+
+export const List = styled.ul``;
+
+export const Item = styled.li``;
