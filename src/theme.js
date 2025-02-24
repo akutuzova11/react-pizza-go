3b370bdfda1fe334f12c9c9cd8b3a22c235bbe85
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-24 21:14:44 +0100

    add colors and breakpoints

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    lightgray: "#F3F3F3",
+    yellow: "#FFDF8C",
+    lightblue: "#74D5DE",
+    darkblue: "#56CAD8",
+    lightorange: "#DE8E68",
+    darkorange: "#FE5F1E",
+    darkgray: "#777777",
+    black: "#000000",
+  },
+
+  breakpoint: {
+    mobileMax: 767,
+  },
+};
