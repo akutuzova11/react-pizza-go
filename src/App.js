9933384f6f179197632a77958921ec022db89b26
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-24 21:20:29 +0100

    Set up app structure with sorting and pizza list components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { Navigation } from "./common/Navigation";
+import { useState } from "react";
+import { PizzaList } from "./common/PizzaList";
 
 function App() {
-  return (
-    <div className="App">
+  const [sortedItems, setSortedItems] = useState([
+    { id: 1, name: "Margherita", price: 8.99 },
+    { id: 2, name: "Pepperoni", price: 10.99 },
+    { id: 3, name: "BBQ Chicken", price: 12.99 },
+  ]);
 
-    </div>
+  return (
+    <>
+      <Navigation setSortedItems={setSortedItems} />
+      <PizzaList sortedItems={sortedItems} />
+    </>
   );
 }
 
