f53dc5de8ab7d1b613d9dd06c1e79eb267220360
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-24 21:16:08 +0100

    implement ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
+import { GlobalStyles } from "./GlobalStyles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
