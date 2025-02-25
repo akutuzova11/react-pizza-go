d407bdc7434cebb156e27e14ec6d183517a586ce
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-25 20:16:56 +0100

    add styling for PizzaList

--- src/common/PizzaList/styled.js
+++ src/common/PizzaList/styled.js
@@ -1,12 +1,22 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  margin-top: 100px;
-  padding: 20px;
+  margin-top: 32px;
+  display: grid;
+grid-template-columns: repeat(auto-fit, minmax(250px, 1fr)); 
+  gap: 32px;
+  justify-items: center;
+  align-items: center;
+  text-align: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.laptop}px) {
+grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  }
 `;
 
-export const Header = styled.h2``;
-
-export const List = styled.ul``;
-
-export const Item = styled.li``;
+export const Header = styled.h1`
+  font-size: 32px;
+  font-weight: 500;
+  letter-spacing: 1%;
+  margin: 16px 0;
+`;
