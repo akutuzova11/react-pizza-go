324be86b8a28b61564d3074bfc64c1e9962f7d10
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-25 20:13:22 +0100

    add route functions for different pizza categories

---
+++ src/routes.js
@@ -0,0 +1,6 @@
+export const toAll = () => "/all";
+export const toMeatLovers = () => "/meatlovers";
+export const toVegetarian = () => "/vegetarian";
+export const toBBQ = () => "/bbq";
+export const toSpicy = () => "/spicy";
+export const toCalzone = () => "/calzone";
