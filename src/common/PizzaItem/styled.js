22e5ec44c1c06485229f12b6ac2a5936d88d35ba
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-26 15:50:03 +0100

    add styling for pizza loader

--- src/common/PizzaItem/styled.js
+++ src/common/PizzaItem/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import pizzaLoader from "../../images/PizzaLoader.gif";
 
 export const PizzaContainer = styled.div`
   display: grid;
@@ -30,7 +31,7 @@ export const Details = styled.div`
   flex-direction: column;
   align-items: center;
   width: 100%;
-  max-wisth: 320px;
+  max-width: 320px;
 `;
 
 export const PizzaTitle = styled.h2`
@@ -50,7 +51,7 @@ export const Selector = styled.div`
   border-radius: 10px;
   padding: 8px;
   width: 100%;
-  width: 300px;
+  width: 250px;
 `;
 
 export const SelectorList = styled.ul`
@@ -92,7 +93,7 @@ export const BottomSection = styled.div`
   align-items: center;
   margin-top: 16px;
   width: 100%;
-  width: 300px;
+  width: 250px;
 `;
 
 export const Price = styled.div`
@@ -101,17 +102,61 @@ export const Price = styled.div`
   letter-spacing: 1.5%;
 `;
 
+export const Quantity = styled.span`
+  margin-left: 8px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.color.white};
+  font-size: 16px;
+  background-color: ${({ theme }) => theme.color.darkorange};
+  border-radius: 15px;
+  padding: 2px 12px;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.white};
+    color: ${({ theme }) => theme.color.darkorange};
+  }
+`;
+
 export const Button = styled.button`
   font-size: 16px;
   font-weight: 500;
   color: ${({ theme }) => theme.color.darkorange};
-  background-color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme, $loading }) =>
+    $loading ? theme.color.darkorange : theme.color.white};
   border: ${({ theme }) => theme.color.darkorange} 1px solid;
   border-radius: 30px;
   padding: 10px 20px;
   cursor: pointer;
-  &:hover{
-  background-color: ${({ theme }) => theme.color.darkorange};
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  width: 140px;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.darkorange};
     color: ${({ theme }) => theme.color.white};
+
+    & ${Quantity} {
+      background-color: ${({ theme }) => theme.color.white};
+      color: ${({ theme }) => theme.color.darkorange};
+    }
   }
+`;
+
+export const LoadingIcon = styled.div`
+  background-image: url(${pizzaLoader});
+  width: 45px;
+  height: 100%;
+  margin: 0;
+  display: block;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
 `;
