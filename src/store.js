2e639ebf4611bcd55a4d2897c25e285816c5f923
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-28 19:43:44 +0100

    set up Redux store with basket and pizza reducers

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import basketReducer from "./features/basketSlice";
+import pizzaReducer from "./features/pizzaSlice";
+
+export const store = configureStore({
+  reducer: {
+    basket: basketReducer,
+    pizza: pizzaReducer,
+  },
+});
