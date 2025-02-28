a50270ca340acae6dc07338463415d7981b8d9fe
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-28 19:43:03 +0100

    add Redux store provider to main entry point

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { theme } from "./theme";
 import { GlobalStyles } from "./GlobalStyles";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
