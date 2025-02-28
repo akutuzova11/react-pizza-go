a245336e3a730ba8e8696f000a7f34124b41a8f4
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-28 19:51:43 +0100

    create useFilteredPizzas custom hook for category-based filtering

---
+++ src/hooks/useFilteredPizzas.js
@@ -0,0 +1,17 @@
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { selectPizzas } from "../features/pizzaSlice";
+import { useLocation } from "react-router-dom";
+import categoryRoutes from "../handleCategoryRoutes";
+
+export const useFilteredPizzas = () => {
+  const location = useLocation();
+  const category = categoryRoutes[location.pathname] || "All";
+  const pizzas = useSelector(selectPizzas);
+
+  return useMemo(() => {
+    return category === "All"
+      ? pizzas
+      : pizzas.filter((pizza) => pizza.category === category);
+  }, [category, pizzas]);
+};
