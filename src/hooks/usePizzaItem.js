a9ec8d163177f362f7aa6087c4e4d2e28aa66fe8
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-28 19:52:01 +0100

    create usePizzaItem custom hook for managing pizza item state and adding to basket

---
+++ src/hooks/usePizzaItem.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToBasket } from "../features/basketSlice";
+
+export const usePizzaItem = ({
+  id,
+  availableTypes,
+  availableSizes,
+  price,
+  name,
+  imageUrl,
+}) => {
+  const dispatch = useDispatch();
+  const [activeType, setActiveType] = useState(0);
+  const [activeSize, setActiveSize] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const onSelectType = (index) => setActiveType(index);
+  const onSelectSize = (index) => setActiveSize(index);
+
+  const onAddPizza = async () => {
+    setLoading(true);
+
+    const pizzaObj = {
+      id,
+      name,
+      imageUrl,
+      price,
+      size: availableSizes[activeSize],
+      type: availableTypes[activeType],
+    };
+    await new Promise((resolve) => setTimeout(resolve, 1200));
+    dispatch(addToBasket(pizzaObj));
+    setLoading(false);
+  };
+
+  return {
+    activeType,
+    activeSize,
+    loading,
+    onSelectType,
+    onSelectSize,
+    onAddPizza,
+  };
+};
