363125793fda7af34e389efb9931f2c5febc8490
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-04 20:52:54 +0100

    fix: opt into React Router v7 future flag for startTransition

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/react-pizza-go">
+      <BrowserRouter
+        basename="/react-pizza-go"
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
         <ThemeProvider theme={theme}>
           <GlobalStyles />
           <App />
